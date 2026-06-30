d8013d546b041ec73b9e5214a1c035441214ac7b
khadija <khadijajjop2@gmail.com>
2026-06-30 19:38:52 +0000

    Initialisation du Tableaux de produits

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$categories=[];
+$categories[]=[
+
+        0 => ['nom'=>'Alimentaire','code'=>'1234','produits'=>[
+
+                            0 =>['nom'=>'Huile','reference'=>'KM34','prix'=>'1200','quantite'=>'100',],
+                            1 =>['nom'=>'Sucre','reference'=>'KM25','prix'=>'600','quantite'=>'50',]
+                                                             ]
+            ],
+            
+        1 => ['nom'=>'Hygyene','code'=>'2864','produits'=>[]]
+
+];
