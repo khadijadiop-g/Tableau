06df9155c9b2062ff321411e397039463920cdd2
khadija <khadijajjop2@gmail.com>
2026-07-01 01:22:42 +0000

    Ajouter une categorie en lui affectants des produits

--- index.php
+++ index.php
@@ -135,3 +135,115 @@ do{
 
 
        print_r($categories);
+
+do {
+$isNomExist = false;
+$nomCategorie =readline("Entrer un nom: ");
+if(empty($nomCategorie)){
+    echo "Le champ ne doit pas etre vide \n";
+    $isNomExist  = true;
+}
+foreach ($categories as $key => $categorie) {
+    if($categorie['nom'] === $nomCategorie){
+        echo "Ce nom de categorie existe deja \n";
+       $isNomExist = true;
+    }
+     
+}
+} while ($isNomExist);
+
+do {
+$isCodeExist = false;
+$codeCategorie =readline("Entrer un code: ");
+if(empty($codeCategorie)){
+    echo "Le champ ne doit pas etre vide \n";
+    $isCodeExist  =true;
+}
+foreach ($categories as $key => $categorie) {
+    if($categorie['code'] === $codeCategorie){
+        echo "Ce code de categorie existe deja \n";
+       $isCodeExist = true;
+    }
+     
+}
+} while ($isCodeExist);
+
+$produits = [];
+
+ do {
+
+ do {
+    $isNomExist = false;
+    $nomProduit =readline("Entrer un nom: ");
+    if(empty($nomProduit)){
+        echo "Le champ ne doit pas etre vide \n";
+        $isNomExist  = true;
+    }
+    foreach ($categories as $key => $categorie) {
+           foreach ($categorie['produits'] as $produit) {
+    if ($produit['nom'] === $nomProduit) {
+        echo "Ce nom de produit existe deja \n";
+        $isNomExist = true;
+        break;
+    }
+}
+        
+    }
+} while ($isNomExist);
+
+do {
+    $isRefExist = false;
+    $reference =readline("Entrer un reference: ");
+    if(empty($reference)){
+        echo "Le champ ne doit pas etre vide \n";
+        $isRefExist  = true;
+    }
+    foreach ($categories as $key => $categorie) {
+           foreach ($categorie['produits'] as $produit) {
+    if ($produit['reference'] === $reference) {
+        echo "Cette référence de produit existe deja \n";
+        $isRefExist = true;
+        break;
+    }
+    }
+        
+    }
+} while ($isRefExist);
+
+do{
+    $prix = (int)readline("Entrer le prix: ");
+    if($prix < 0){
+        echo "Le prix doit etre positive \n";
+    }
+
+}while($prix < 0);
+
+do{
+    $quantite = (int)readline("Entrer le quantite: ");
+    if($quantite < 0){
+        echo "La quantite doit etre positive \n";
+    }
+
+}while($quantite < 0);
+
+         $produit =   [
+                    'nom' => $nomProduit,
+                    'reference' => $reference,
+                    'prix' => $prix,
+                    'quantite' => $quantite
+                  ];
+          $produits[]= $produit;
+
+          $choix = strtolower(readline(" voulez vous continuer  oui/non "));
+          
+     } while ($choix === "oui");
+
+     $categorie  =   [
+         'nom' => $nomCategorie,
+            'code' => $code,
+            'produits' =>  $produits 
+         ];
+
+         $categories[] = $categorie;
+
+         print_r($categories);
